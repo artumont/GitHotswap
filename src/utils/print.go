42649f3b9eefc772feca7fd372c3c2548d690a40
artumont <197291181+artumont@users.noreply.github.com>
2025-03-31 09:46:04 -0600

    refactor: Improved logging utilities to use formatted output with colorized prefixes

--- src/utils/print.go
+++ src/utils/print.go
@@ -1,12 +1,27 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/fatih/color"
 )
 
-var (
-	Success = color.New(color.FgGreen).PrintlnFunc()
-	Info    = color.New(color.FgCyan).PrintlnFunc()
-	Error   = color.New(color.FgRed).PrintlnFunc()
-	Warning = color.New(color.FgYellow).PrintlnFunc()
-)
+func Info(args ...any) {
+	fmt.Printf("%s %s", color.GreenString("🛈"), color.WhiteString(fmt.Sprint(args...)))
+}
+
+func Success(args ...any) {
+	fmt.Printf("%s %s", color.GreenString("✓"), color.WhiteString(fmt.Sprint(args...)))
+}
+
+func Warning(args ...any) {
+	fmt.Printf("%s %s", color.YellowString("⚠"), color.WhiteString(fmt.Sprint(args...)))
+}
+
+func Error(args ...any) {
+	fmt.Printf("%s %s", color.RedString("✗"), color.WhiteString(fmt.Sprint(args...)))
+}
+
+func Debug(args ...any) {
+	fmt.Printf("%s %s", color.CyanString("⚙"), color.WhiteString(fmt.Sprint(args...)))
+}
