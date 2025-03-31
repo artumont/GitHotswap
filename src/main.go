a8060cefc4222b8052a4e65cbcdf03ab6fe05dd7
artumont <197291181+artumont@users.noreply.github.com>
2025-03-31 10:03:24 -0600

    refactor: Comment out command handling and configuration loading in main.go (temporary)

--- src/main.go
+++ src/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/artumont/GitHotswap/src/commands"
+	//"github.com/artumont/GitHotswap/src/commands"
 	"github.com/artumont/GitHotswap/src/utils"
 )
 
@@ -16,34 +16,14 @@ func main() {
 		os.Exit(0)
 	}
 
-	config, err := utils.LoadConfig()
-	if err != nil {
-		utils.Error("Error loading config:", err)
-		os.Exit(0)
-	}
+	//config, err := utils.LoadConfig()
+	//if err != nil {
+	//	utils.Error("Error loading config:", err)
+	//	os.Exit(0)
+	//}
 
 	switch args[0] {
-	case "swap":
-		if len(args) < 1 {
-			utils.Info("Usage: git-hotswap swap <profile_name>")
-			os.Exit(0)
-		}
-		commands.SwapHandler(ProcessParams(args[1:]), config)
-	case "profile":
-		if len(args) < 2 {
-			utils.Info("Usage: git-hotswap profile <operation> [options]")
-			os.Exit(0)
-		}
-		operation := args[1]
-		commands.ProfileHandler(operation, ProcessParams(args[1:]), config)
-	case "help":
-		utils.Info("Usage: git-hotswap <command> [options]")
-		utils.Info("Commands:")
-		utils.Info("  swap [profile_name]              Swap to the specified profile (profile_name is optional if only two profiles exist)")
-		utils.Info("  profile <operation> [options]    Manage profiles (add, remove, list)")
-		utils.Info("  help                             Show this help message")
-	default:
-		utils.Warning("Unknown command:", args[0], "| use git-hotswap help for help")
+
 	}
 }
 
